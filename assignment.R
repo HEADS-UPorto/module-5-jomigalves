ac8e2b91400d806f8b475c2b94e34e293dc3b929
jomigalves <jma.alves@zoho.com>
2020-11-30 00:24:53 +0000

    Submission #1

--- assignment.R
+++ assignment.R
@@ -12,7 +12,22 @@ DBI::dbListTables(con)
 
 #################### Start your code here ######################
 
+# make a pointer to a database table
+stock_tbl <- tbl(con, "stock")
+rep_tbl <- tbl(con, "representers")
 
+# sum totals by Representer
+stock_tbl %>%
+  select(Rep, Total) %>%
+  group_by(Rep) %>%
+  summarise(sum = sum(Total, na.rm = TRUE)) %>%
+  arrange(desc(sum)) %>%
+  collect()
+
+#retrieve address as 'dataset'
+dataset <- rep_tbl %>%
+  filter(Rep == "Kivell") %>%
+  collect()
 
 #################### End it here ######################
 
